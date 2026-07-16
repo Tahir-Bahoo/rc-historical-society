214b6f1e5195f5dd7a5c6821489e2dccc642c9e9
Abubakar Mehmood <abubakarmahmood25@gmail.com>
2026-07-16 19:43:30 +0500

    changes

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
     host.strip()
     for host in os.environ.get(
         "DJANGO_ALLOWED_HOSTS",
-        "localhost,127.0.0.1,testserver,52.15.199.202",
+        "localhost,127.0.0.1,testserver,52.15.199.202,3.138.112.5",
     ).split(",")
     if host.strip()
 ]
